499d3fc1166004706b9197df840dc7d48c411205
favoyang <favoyang@users.noreply.github.com>
2024-10-24 11:49:39 +0000

    Deploying to gh-pages from @ openupm/openupm-next@d5df2f69521a24aae1c604cf8acfa4c881137735 🚀

---
+++ assets/index.html-DUVRUC-i.js
@@ -0,0 +1,20 @@
+import{_ as e,o as t,c as a,a as n}from"./app-Cu83vo63.js";const s={},o=n(`<h3 id="get-started-with-cli-optional" tabindex="-1"><a class="header-anchor" href="#get-started-with-cli-optional"><span>Get Started with CLI (Optional)</span></a></h3><div class="language-bash" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token comment"># Install openupm-cli</span>
+$ <span class="token function">npm</span> <span class="token function">install</span> <span class="token parameter variable">-g</span> openupm-cli
+<span class="token comment"># OR yarn global add openupm-cli</span>
+
+<span class="token comment"># Enter your Unity project folder</span>
+$ <span class="token builtin class-name">cd</span> YOUR_UNITY_PROJECT_FOLDER
+
+<span class="token comment"># Search a package</span>
+$ openupm search addressable-importer
+┌──────────────────────────────────────────┬────────────────────┬────────────┐
+│ Name                                     │ Version            │ Date       │
+├──────────────────────────────────────────┼────────────────────┼────────────┤
+│ com.littlebigfun.addressable-importer    │ <span class="token number">0.16</span>.1             │ <span class="token number">2023</span>-02-08 │
+└──────────────────────────────────────────┴────────────────────┴────────────┘
+
+<span class="token comment"># Install package</span>
+$ openupm <span class="token function">add</span> com.littlebigfun.addressable-importer
+added: com.littlebigfun.addressable-importer@0.4.1
+manifest updated, please <span class="token function">open</span> unity project to apply changes
+</code></pre></div><div class="custom-container tip"><p class="custom-container-title">DISCLAIMER</p><p>OpenUPM is an autonomous, open-source service and is not associated with Unity Technologies Inc.</p></div>`,3),i=[o];function c(p,r){return t(),a("div",null,i)}const m=e(s,[["render",c],["__file","index.html.vue"]]),d=JSON.parse('{"path":"/","title":"Open Source Unity Package Registry","lang":"en-US","frontmatter":{"layout":"HomeLayout","showFooter":true,"editLink":false,"title":"Open Source Unity Package Registry","description":"OpenUPM is a managed UPM registry with automatic build services for open-source Unity packages.","heroText":"Open Source Unity Package Registry","features":[{"title":"Open Source Packages","desc":"Hosts over <%= package_count %> open source packages, carefully curated by the community."},{"title":"Automated Package Publishing","desc":"Automatically tracks Git tags to streamline the publishing process and ensure packages are always up-to-date."},{"title":"Command-Line Interface","desc":"Empower command-line lovers with the OpenUPM-CLI tool for seamless UPM registry interaction."}],"head":[["link",{"rel":"canonical","href":"https://openupm.com/"}],["meta",{"property":"og:url","content":"https://openupm.com/"}],["meta",{"property":"og:site_name","content":"OpenUPM"}],["meta",{"property":"og:title","content":"Open Source Unity Package Registry"}],["meta",{"property":"og:description","content":"OpenUPM is a managed UPM registry with automatic build services for open-source Unity packages."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:image","content":"https://openupm.com/images/openupm-twitter.png"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"name":"twitter:card","content":"summary_large_image"}],["meta",{"name":"twitter:image:alt","content":"Open Source Unity Package Registry"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"Open Source Unity Package Registry\\",\\"image\\":[\\"https://openupm.com/images/openupm-twitter.png\\"],\\"dateModified\\":null,\\"author\\":[]}"]]},"headers":[{"level":3,"title":"Get Started with CLI (Optional)","slug":"get-started-with-cli-optional","link":"#get-started-with-cli-optional","children":[]}],"filePathRelative":"index.md"}');export{m as comp,d as data};
